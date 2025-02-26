f73a4f032ff8fdf06b1b01a99dd34e18e118dfa3
Ben Sheres <thetruenalm2@gmail.com>
2025-02-26 17:23:34 -0500

    Lab 2

---
+++ global.js
@@ -0,0 +1,89 @@
+console.log("IT's ALIVE");
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact Info" },
+  { url: "resume/", title: "CV" },
+  {
+    url: "https://github.com/therealnalm",
+    title: "Github",
+  },
+];
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+	<label class='color-scheme'>
+		Theme:
+		<select>
+			<option value = "dark">Dark</option>
+            <option value = "light">Light</option>
+            <option value = "light dark">Automatic</option>
+		</select>
+	</label>`
+);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  if (!ARE_WE_HOME && !url.startsWith("http")) {
+    url = "../" + url;
+    if (url == "https://github.com/therealnalm") {
+      //Handling image routing for github logo on github pages
+      title = `<img
+          src="images/githublogo.png"
+          alt="Github logo"
+          height="20"
+          width="20"
+      />`;
+    }
+  }
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+  if (a.host !== location.host) {
+    a.target = "blank";
+  }
+  nav.append(a);
+}
+
+let select = document.querySelector("select");
+
+select.addEventListener("input", function (event) {
+  document.documentElement.style.setProperty(
+    "color-scheme",
+    event.target.value
+  );
+  localStorage.colorScheme = event.target.value;
+});
+
+if (localStorage.colorScheme) {
+  document.documentElement.style.setProperty(
+    "color-scheme",
+    localStorage.colorScheme
+  );
+  select.value = localStorage.colorScheme;
+}
+
+// let navLinks = $$("nav a");
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// let test = $$("*");
+// console.log(navLinks);
+// console.log(navLinks[0].pathname + "  " + location.pathname);
+
+// currentLink?.classList.add("current");
